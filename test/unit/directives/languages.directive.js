00fd6625c476ce3de1af0fab13b27de5ca955857
Nadzeya Shedava <Nadzeya_Shedava@epam.com>
2016-10-02 01:19:11 +0300

    Move files to subfolders

---
+++ test/unit/directives/languages.directive.js
@@ -0,0 +1,45 @@
+'use strict';
+
+describe('languagesDirective', function () {
+	var element, scope,
+		i18nService;
+
+	beforeEach(module('app'));
+
+	beforeEach(module(function ($provide) {
+		i18nService = {
+			getLanguages: function () {
+			}
+		};
+		spyOn(i18nService, 'getLanguages').andReturn(['en', 'fr']);
+		$provide.value('i18nService', i18nService);
+	}));
+
+	beforeEach(inject(function ($rootScope, $compile) {
+		scope = $rootScope.$new();
+		element = '<languages value="l"></languages>';
+		element = $compile(element)(scope);
+		scope.$digest();
+	}));
+
+	it('should contains all languages', function () {
+		var isolateScope = element.isolateScope();
+
+		expect(i18nService.getLanguages).toHaveBeenCalled();
+		expect(isolateScope.languages).toEqual(['en', 'fr']);
+	});
+
+	it('should english be default language', function () {
+		var isolateScope = element.isolateScope();
+
+		expect(isolateScope.selectedLanguage).toBe('en');
+	});
+
+	it('should have possibility to set language', function () {
+		var isolateScope = element.isolateScope();
+
+		scope.l = 'fr';
+		scope.$apply();
+		expect(isolateScope.selectedLanguage).toBe('fr');
+	});
+});

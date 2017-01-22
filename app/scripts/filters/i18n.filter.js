46834907ce087bc44c7801f7cef1e313aed76ec2
Nadzeya.Shedava <Nadzeya_Shedava@epam.com>
2017-01-23 00:00:27 +0300

    Make files like ES6

--- app/scripts/filters/i18n.filter.js
+++ app/scripts/filters/i18n.filter.js
@@ -1,9 +1,9 @@
-'use strict';
+let i18nService;
+let I18nFilterFn = (text, language) => i18nService.getText(text, language);
 
-var I18nFilter = ['i18nService', function (i18nService) {
-	return function (text, language) {
-		return i18nService.getText(text, language);
-	};
-}];
+I18nFilter.$inject = ['i18nService'];
 
-export default I18nFilter;
+export default function I18nFilter(__i18nService__) {
+	i18nService = __i18nService__;
+	return I18nFilterFn;
+}
--- app/scripts/services/i18n.service.js
+++ app/scripts/services/i18n.service.js
@@ -1,41 +1,45 @@
 'use strict';
 
-var I18nService = ['$http', function ($http) {
-	var promise,
-		languages = [],
-		language,
-		texts;
+let $http,
+	promise,
+	languages = [],
+	language,
+	texts;
 
-	return {
-		getLanguage: function () {
-			return language;
-		},
-		setLanguage: function(lang) {
-			if (lang && texts[lang]) {
-				language = lang;
-			}
-		},
-		getLanguages: function () {
-			return promise || requestLanguages();
-		},
-		getText: function (text, requestedLanguage) {
-			var l = requestedLanguage || language;
-			return texts && texts[l][text] ? texts[l][text] : '';
-		}
-	};
+function requestLanguages(){
+	promise = $http.get('/translations.json').then((response) => {
+		var data = response.data;
 
-	function requestLanguages(){
-		promise = $http.get('/translations.json').then(function (response) {
-			var data = response.data;
+		languages = languages.concat(Object.keys(data));
+		language = languages[0];
+		texts = data;
 
-			Array.prototype.push.apply(languages, Object.keys(data));
-			language = languages[0];
-			texts = data;
+		return languages;
+	});
+	return promise;
+}
 
-			return languages;
-		});
-		return promise;
+class I18nService {
+	constructor(__$http__) {
+		$http = __$http__;
 	}
-}];
+	getLanguage () {
+		return language;
+	}
+	setLanguage (lang) {
+		if (lang && texts[lang]) {
+			language = lang;
+		}
+	}
+	getLanguages () {
+		return promise || requestLanguages();
+	}
+	getText (text, requestedLanguage) {
+		var l = requestedLanguage || language;
+		return texts && texts[l][text] ? texts[l][text] : '';
+	}
+}
+
+I18nService.$inject = ['$http'];
 
 export default I18nService;
